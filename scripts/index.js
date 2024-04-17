4b49a6899653ce889e5a1d368350bbf23affbe88
Daniil Derid <derid.daniil@yandex.ru>
2024-04-17 19:16:45 +0500

    feat JS: added function to show cards and delete them

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+function viewCards() {
+  let cardElement = [];
+
+  for (let i = 0; i < initialCards.length; i++) {
+    cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__image').src = initialCards[i].link;
+    cardElement.querySelector('.card__title').textContent = initialCards[i].name;
+    
+    cardElement.querySelector('.card__delete-button').addEventListener('click', removeCard);
+
+    placesList.append(cardElement);
+  }
+}
+
+function removeCard(evt) {
+  let target = evt.target.closest('.card');
+  target.remove();
+}
+
+viewCards();
